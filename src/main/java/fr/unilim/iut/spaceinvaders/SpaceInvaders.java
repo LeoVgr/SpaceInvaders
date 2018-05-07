0a06ddb10772be1e7622312d9e9cbd0519160e15
Viguier <leo.vgr@hotmail.com>
2018-05-07 10:16:43 +0200

    Maj début de séance TP2

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -3,20 +3,32 @@ package fr.unilim.iut.spaceinvaders;
 public class SpaceInvaders {
 	int longueur;
 	int hauteur;
+	Vaisseau vaisseau;
 	
 	public SpaceInvaders(int longeur, int hauteur) {
 		this.longueur = longeur;
 		this.hauteur = hauteur;
 	}
+	
 	@Override
 	public String toString() {
+		
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i =0; i<hauteur;i++) {
-			for(int j =0;j<longueur; j++) {
-				espaceDeJeu.append('.');
+		for (int y =0; y<hauteur;y++) {
+			for(int x =0;x<longueur; x++) {
+				if (vaisseau.occuperLaPosition(x,y)){
+					espaceDeJeu.append('V');
+				}else {
+					espaceDeJeu.append('.');
+				}
+				
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
+	}
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		this.vaisseau = new Vaisseau(x,y);
+		
 	}
 }
