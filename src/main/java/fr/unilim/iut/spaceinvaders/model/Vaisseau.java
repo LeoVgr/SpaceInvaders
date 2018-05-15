a1341beb53f523b000cdb16d6247f567d79c2a25
Lara <lv.laravidoni@gmail.com>
2018-05-15 17:48:47 +0200

    refactor abscisse et deplacer gauche/droite

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Vaisseau.java
@@ -1,29 +1,49 @@
 package fr.unilim.iut.spaceinvaders.model;
 
 public class Vaisseau {
-	int x;
-	int y;
-	
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
 
-	public boolean occupeLaPosition(int x2, int y2) {
-		return(this.x==x2) && (this.y==y2);
-	}
+    int x;
+    int y;
+    int longueur;
+    int hauteur;
 
+    public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
+	}
+    public Vaisseau(int longueur, int hauteur, int x, int y) {
+	   this.longueur=longueur;
+	   this.hauteur=hauteur;
+	   this.x = x;
+	   this.y = y;
+    }
+    
 	public void seDeplacerVersLaDroite() {
 		this.x=this.x+1;
 		
 	}
 
-	public int abscisse() {
+	public int abscisseLaPlusAGauche() {
 		return this.x;
 	}
 
 	public void seDeplacerVersLaGauche() {
 		this.x=this.x-1;
+		
+	}
+	public boolean occupeLaPosition(int x, int y) {
+	     if ((abscisseLaPlusAGauche()<=x) && (x<=abscisseLaPlusADroite())) 
+		      if ( (this.y-this.hauteur+1<=y) && (y<=this.y))
+			  return true;
+		
+	     return false;
+    }
+
+	public int abscisseLaPlusADroite() {
+		return abscisseLaPlusAGauche()+this.longueur-1 ;
+	}
+	public void positionner(int x, int y) {
+		this.x=x;
+		this.y=y;
 		
 	}
 }
