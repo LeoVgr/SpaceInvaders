307231d71843ce6c7e47c38fd5d05cd3a58ff25d
Lara <lv.laravidoni@gmail.com>
2018-05-18 18:41:50 +0200

    Seance 2 Etape 2 (finie)
    Nouvelle classe : DebordementEspaceJeuException

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import fr.unilim.iut.spaceinvaders.model.SpaceInvaders;
+import fr.unilim.iut.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvadersTest {
@@ -171,4 +172,19 @@ public class SpaceInvadersTest {
 			".......VVV.....\n" + 
 			".......VVV.....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
 		}
+	 
+	 @Test
+	 public void test_UnNouveauVaisseauPositionneDansEspaceJeuMaisAvecDimensionTropGrande_DoitLeverUneExceptionDeDebordement() {
+	 try {
+	 spaceinvaders.positionnerUnNouveauVaisseau(9,2,7,9);
+	 fail("Dépassement du vaisseau à droite en raison de sa longueur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+	 } catch (final DebordementEspaceJeuException e) {
+	 }
+	 try {
+	 spaceinvaders.positionnerUnNouveauVaisseau(3,4,7,1);
+	 fail("Dépassement du vaisseau vers le haut en raison de sa hauteur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+	 } catch (final DebordementEspaceJeuException e) {
+	 }
+	 }
+
 }
